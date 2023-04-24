c48e92081c83da7c4240c3c1594ed6f42917bb9d
CristianRicardoLeite <cristianricardoleite@gmail.com>
2023-04-24 11:09:25 -0300

    Routes changes

---
+++ frontend-code-burger/src/routes/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Navigate, Outlet } from 'react-router-dom'
+import { Header } from '../components'
+
+function PrivateRoutes () {
+  const user = localStorage.getItem('codeburger:userData')
+
+  if (!user) {
+    return <Navigate to='/login'/>
+  }
+
+  if (!user.admin) {
+    return <Navigate to='/'/>
+  }
+
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    </>
+  )
+}
+
+export default PrivateRoutes
--- frontend-code-burger/src/routes/routes.js
+++ frontend-code-burger/src/routes/routes.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home, Produts, Login, Register, Cart } from '../containers'
+import PrivateRoutes from './PrivateRoute'
+import Admin from '../containers/admin'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/produtos" element={<Produts />} />
-        <Route path="/carrinho" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Register />} />
+        <Route element={<PrivateRoutes/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/produtos" element={<Produts />} />
+          <Route path="/carrinho" element={<Cart />} />
+          <Route path="/admin" element={<Admin />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
