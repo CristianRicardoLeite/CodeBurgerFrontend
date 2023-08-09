d64cc283123ed9b450eac41505786967764696f5
CristianRicardoLeite <cristianricardoleite@gmail.com>
2023-08-08 21:14:17 -0300

    change baseURL to railway

--- frontend-code-burger/src/services/api.js
+++ frontend-code-burger/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://railway.app/project/76e5d1f3-48c4-4bdb-a712-5b4caa5a5c9c/service/5c838709-d5ac-4dd8-b3ad-20fd816a8db2?id=25685f33-8ebd-42ca-aa6e-31dcd4f0f3a7'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
